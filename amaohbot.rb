9b83d509d782820ecc93faacfb9297ee6f941d30
syouhei3611 <syouhei3611@hotmail.com>
2016-02-19 14:46:21 +0900

    Create amaohbot.rb

---
+++ amaohbot.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+require 'twitter'
+
+client = Twitter::REST::Client.new(
+	consumer_key:		 ENV['TWITTER_CONSUMER_KEY'],
+	consumer_secret:	 ENV['TWITTER_CONSUMER_SECRET'],
+	access_token:		 ENV['TWITTER_ACCESS_TOKEN'],
+	access_token_secret: ENV['TWITTER_ACCESS_TOKEN_SECRET']
+)
+
+now = Time.now
+since = Time.local(2016, 2, 18)
+day = (now - since).div(60*60*24) + 12
+# p day
+
+# get = client.user()
+# p get
+rep = "あまおうリキュールDay#{day}"
+client.update_profile({name: rep})
+print("update name \'#{get}\' to \'#{rep}\'\n")
+# print("update name to \'#{rep}\'\n")
